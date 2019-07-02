76ed26ad031e689bc9ae0d757665a93a60c4115d
LightSun <donshine723@gmail.com>
2019-07-03 00:34:01 +0800

    make name

---
+++ Tree/src/test/java/com/heaven7/java/tree/MakeName.java
@@ -0,0 +1,21 @@
+package com.heaven7.java.tree;
+
+/*
+取名字：
+      1，五行
+      2， 字意-- wait
+      3，声母，韵母，音调。 Pinyin4j
+      4, 结构() -- wait
+      5, 建筑美  -- wait
+      6, 出处高雅 -- 诗词。楚辞，诗经
+            view-source:https://www.gushiwen.org/gushi/tangshi.aspx (需要爬数据)
+      7. 五格三才。 -- wait.
+         http://www.360doc.com/content/18/0414/14/4153217_745576219.shtml
+
+     忌讳：
+        意思偏好： http://www.sohu.com/a/212424204_99934817
+
+
+ */
+public class MakeName {
+}
