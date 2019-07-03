00ba26dca28ed7f6b61023dc0a355f49b6f85fea
LightSun <donshine723@gmail.com>
2019-07-04 00:37:24 +0800

    ok

--- Tree/src/test/java/com/heaven7/java/tree/ClassicalParser.java
+++ Tree/src/test/java/com/heaven7/java/tree/ClassicalParser.java
@@ -9,12 +9,18 @@ import java.net.URL;
 
 public class ClassicalParser {
 
+    /**
+     * 1, 解析成每个节点。
+     * 2，每个类型类似 view-source:https://so.gushiwen.org/gushi/yongwu.aspx
+     * 3, 具体的诗类似 view-source:https://so.gushiwen.org/shiwenv_53b46be5f768.aspx
+     */
     public void start(){
         URL url = getClass().getResource("/classical_types.xml");
         System.out.println(url);
         try {
             InputStream in = url.openStream();
             new ReflectyIo().xml()
+                    //todo type token
                     .read(new InputStreamReader(in));
         } catch (IOException e) {
             throw new RuntimeException(e);
